a749e6ac90ae1a1a9f849411c730746fe35fe337
Jeremy E Kozdon <jekozdon@nps.edu>
2013-06-10 17:11:19 -0700

    moving test to a function

--- tests/bfam_test_timestep_lsrk.c
+++ tests/bfam_test_timestep_lsrk.c
@@ -1,46 +1,36 @@
 #include <bfam.h>
 #include <bfam_subdomain_dummy.h>
 
-int
-main (int argc, char *argv[])
+void
+test_integration(int N, bfam_ts_lsrk_method_t method)
 {
   bfam_ts_lsrk_t* ts;
   bfam_domain_t* dom = bfam_domain_new(NULL);
-  bfam_subdomain_dummy_t* subDom = bfam_subdomain_dummy_new("a",4);
+  bfam_subdomain_dummy_t* subDom = bfam_subdomain_dummy_new("a",N);
   bfam_domain_add_subdomain(dom, (bfam_subdomain_t*) subDom);
 
   const char *tags[]   = {"_subdomain_dummy", NULL};
   const char *fields[] = {"q",NULL};
 
-  // printf("%e\n",(double) bfam_subdomain_dummy_exact(subDom, 1.0));
-  // printf("%e\n",(double) bfam_subdomain_dummy_exact(subDom, 0.2));
+  bfam_domain_add_fields(dom,BFAM_DOMAIN_AND,tags,fields);
 
-  /* test Kennedy-Carpenter(5,4) */
-  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_KC54,
+  ts = bfam_ts_lsrk_new(dom,method,
       tags,BFAM_DOMAIN_AND,fields, NULL, NULL, NULL);
+
   bfam_ts_lsrk_free(ts);
   bfam_free(ts);
-
-  /* test Williamson(3,3) */
-  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_W33,
-      tags,BFAM_DOMAIN_AND,fields, NULL, NULL, NULL);
-  bfam_ts_lsrk_free(ts);
-  bfam_free(ts);
-
-  /* test Heun */
-  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_HEUN,
-      tags,BFAM_DOMAIN_AND,fields, NULL, NULL, NULL);
-  bfam_ts_lsrk_free(ts);
-  bfam_free(ts);
-
-  /* test Forward Euler */
-  ts = bfam_ts_lsrk_new(dom,BFAM_TS_LSRK_FE,
-      tags,BFAM_DOMAIN_AND,fields, NULL, NULL, NULL);
-  bfam_ts_lsrk_free(ts);
-  bfam_free(ts);
-
   bfam_domain_free(dom);
   bfam_free(dom);
+}
+
+int
+main (int argc, char *argv[])
+{
+
+  test_integration(4,BFAM_TS_LSRK_KC54);
+  test_integration(3,BFAM_TS_LSRK_W33 );
+  test_integration(2,BFAM_TS_LSRK_HEUN);
+  test_integration(1,BFAM_TS_LSRK_FE  );
 
   return EXIT_SUCCESS;
 }
